013df298085386c25e5ccbcb6d12e5b7175e803d
archlich <archlich@outlook.com>
2019-09-14 15:13:48 -0400

    1.[CHG]add paging

--- Vic.SportsStore.WebApp/Controllers/ProductController.cs
+++ Vic.SportsStore.WebApp/Controllers/ProductController.cs
@@ -10,7 +10,7 @@ namespace Vic.SportsStore.WebApp.Controllers
     public class ProductController : Controller
     {
         // GET: Product
-
+        public int PageSize = 2;
         public IProductsRepository ProductsRepository { get; set; }
 
         public ActionResult Index()
@@ -18,9 +18,14 @@ namespace Vic.SportsStore.WebApp.Controllers
             return View();
         }
 
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(ProductsRepository.Products);
+            return View(ProductsRepository
+                .Products
+                .OrderBy(p => p.ProductId)//set order by property
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize)
+                );
         }
     }
 }
