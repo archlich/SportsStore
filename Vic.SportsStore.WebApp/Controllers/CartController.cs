1be55fc695a0c1dcace982b888fbb982b9d5a83c
archlich <archlich@outlook.com>
2019-09-28 16:58:17 -0400

    1.[CHG]add Checkout.cshtml

--- Vic.SportsStore.WebApp/Controllers/CartController.cs
+++ Vic.SportsStore.WebApp/Controllers/CartController.cs
@@ -60,5 +60,9 @@ namespace Vic.SportsStore.WebApp.Controllers
         {
             return PartialView(cart);
         }
+        public ViewResult Checkout()
+        {
+            return View(new ShippingDetails());
+        }
     }
 }
