610be9f50e73e605f7d350e98c755e632591edd9
archlich <archlich@outlook.com>
2019-09-28 17:06:33 -0400

    1.[CHG]add display name to check out page

--- Vic.SportsStore.Domain/Entities/ShippingDetails.cs
+++ Vic.SportsStore.Domain/Entities/ShippingDetails.cs
@@ -14,10 +14,15 @@ namespace Vic.SportsStore.Domain.Entities
 
 
         [Required(ErrorMessage = "Please enter the first address line")]
+        [Display(Name = "Line 1")]
         public string Line1 { get; set; }
 
 
+        [Display(Name = "Line 2")]
         public string Line2 { get; set; }
+
+
+        [Display(Name = "Line 3")]
         public string Line3 { get; set; }
 
 
@@ -27,14 +32,9 @@ namespace Vic.SportsStore.Domain.Entities
 
         [Required(ErrorMessage = "Please enter a state name")]
         public string State { get; set; }
-
-
         public string Zip { get; set; }
-
         [Required(ErrorMessage = "Please enter a country name")]
         public string Country { get; set; }
-
-
         public bool GiftWrap { get; set; }
     }
 }
